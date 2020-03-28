5b2d7c67168619528f169b7e10d8a0a195c14d10
guhborda <guh_borda@hotmail.com>
2020-03-28 01:36:23 -0300

    floating btn action working

--- public/components/mobileView.php
+++ public/components/mobileView.php
@@ -9,10 +9,9 @@ function mobileView($params){
     <div class="fixed-action-btn" style="bottom: 0px; right: 10px;">
     <a class="btnFlutuante btn-floating btn-lg btn-default"><i class="fas fa-bolt"></i></a>
       <ul class="list-unstyled">
-        <li><a class="btn-floating red"><i class="fas fa-star"></i></a></li>
-        <li><a class="btn-floating yellow darken-1"><i class="fas fa-user"></i></a></li>
-        <li><a class="btn-floating green"><i class="fas fa-envelope"></i></a></li>
-        <li><a class="btn-floating blue"><i class="fas fa-shopping-cart"></i></a></li>
+        <li ><span class="popFloating"> Cartão </span> <a href="" id="Cartão" class="floatBtn btn-floating purple darken-1"><i class="fas fa-user"></i></a></li>
+        <li ><span class="popFloating"> Despesa </span><a href="" id="Despesa" class="floatBtn btn-floating red"><i class="fas fa-envelope"></i></a></li>
+        <li ><span class="popFloating"> Receita </span><a href="" id="Receita" class="floatBtn btn-floating blue"><i class="fas fa-shopping-cart"></i></a></li>
       </ul>
     </div>
       <script>
@@ -33,6 +32,16 @@ function mobileView($params){
                 btnstatus = 0;
               }
           });
+          
+      $('.floatBtn.btn-floating').click(function(e){
+        e.preventDefault();
+        var btn = $(this);
+        //console.log(btn);
+        var action = btn.attr('id');
+        console.log(action);
+        alert(action);
+      });
+          
         });
         </script>
     <?php
